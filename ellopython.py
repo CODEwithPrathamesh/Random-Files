500e772cd9c3b6cf39625bcd2a94a6f1506fb547
CODEwithPrathamesh <prathamesh.shendarkar@gmail.com>
2018-09-13 19:24:23 +0530

    This is my first commit for python

---
+++ ellopython.py
@@ -0,0 +1,30 @@
+print("Hello World")
+
+animal = "Dog"
+animals = ["Dog", "Cat", "Python"];
+for i in animals:
+    print(i)
+
+i = 5;
+print(i)
+
+while i>0:
+    print(i);
+    i -= 1
+
+# Check it
+if i != 0:
+    print("Yaay")
+else:
+    print("Booo")
+
+# This is my if-else-if loop
+j = 6
+if j != 6:
+    print("Yups")
+elif j == 6:
+    print("Heyy")
+else: 
+    print("Ka BOom");
+
+# Hey Its cool to be commented
